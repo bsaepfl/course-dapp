63b88da577b1965bfb143bf8df611f9726f63f77
Louis Merlin <louis.merlin@epfl.ch>
2018-10-19 13:03:11 +0200

    Comment out smart contract

--- contracts/BSACourse.sol
+++ contracts/BSACourse.sol
@@ -1,44 +1,68 @@
 pragma solidity ^0.4.24;
 
 contract BSACourse {
+  /* #n1
   string public name;
   uint public credits;
   address public university;
-  bool public ended;
+  */
+  
+  /* #n2
   uint public numberOfAttendants;
   uint public numberOfRecipients;
   mapping(address => bool) public attendants;
   mapping(address => bool) public recipients;
+  */
   
+  /* #n3
+  bool public ended;
+  */
+  
+  /* #n1
   constructor(string _name, uint _credits) public {
     university = msg.sender;
     name = _name;
     credits = _credits;
   }
+  */
   
+  /* #n3
   modifier onlyUniversity() {
     require(msg.sender == university);
     _;
   }
+  */
   
+  /* #n3
   modifier stillOpen() {
     require(!ended);
     _;
   }
+  */
   
-  function enroll() public stillOpen() {
+  /* #n2
+  function enroll() public 
+  // #n3 stillOpen()
+  {
     require(!attendants[msg.sender]);
     attendants[msg.sender] = true;
     numberOfAttendants++;
   }
+  */
   
-  function pass(address student) public onlyUniversity() stillOpen() {
+  /* #n2
+  function pass(address student) public onlyUniversity()
+  // #n3 stillOpen()
+  {
     require(attendants[student]);
     recipients[student] = true;
     numberOfRecipients++;
   }
+  */
   
+  /* #n3
   function close() public onlyUniversity() stillOpen() {
     ended = true;
   }
+  */
 }
